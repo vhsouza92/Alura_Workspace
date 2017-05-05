dd389bedf21b6004dc1973414af8aa987a509f75
Victor Hugo <vhsouza92@gmail.com>
2017-05-05 19:59:24 +0000

    Aula-10-services-finish

--- Angular/alurapic/public/js/controllers/foto-controller.js
+++ Angular/alurapic/public/js/controllers/foto-controller.js
@@ -1,17 +1,15 @@
-angular.module('alurapic').controller('FotoController', function($scope, $http, $routeParams){
+angular.module('alurapic').controller('FotoController', function($scope, recursoFoto, $routeParams){
     
     $scope.foto = {};
     $scope.mensagem = "";
     
     if($routeParams.fotoId){
-        $http.get('v1/fotos/'+ $routeParams.fotoId)
-            .success(function(foto){
+        recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto) {
                 $scope.foto = foto;
-            })
-            .error(function(erro){
+        },function(erro){
                 console.log(erro);
                 $scope.mensagem = "Não foi possível localizar foto.";
-            });
+        });
     }
     
     $scope.submeter = function(){
@@ -19,25 +17,21 @@ angular.module('alurapic').controller('FotoController', function($scope, $http, 
             
             // Alteração????
             if($routeParams.fotoId) { 
-                $http.put('/v1/fotos/' + $scope.foto._id, $scope.foto)
-                    .success(function() {
-                        $scope.mensagem = 'Foto "' +$scope.foto.titulo +'" foi alterada com sucesso';
-                        //$scope.foto = {};
-                    })
-                    .error(function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível alterar a foto: "'+ $scope.foto.titulo+'"';
-                    });
+                recursoFoto.update({fotoId: $scope.foto._id},$scope.foto, function(){
+                    $scope.mensagem = 'Foto "' +$scope.foto.titulo +'" foi alterada com sucesso';
+                    //$scope.foto = {};
+                },function(erro) {
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível alterar a foto: "'+ $scope.foto.titulo+'"';
+                });
             } else { 
-                $http.post('/v1/fotos', $scope.foto)
-                    .success(function() {
-                        $scope.mensagem = 'Foto "'+$scope.foto.titulo+'" cadastrada com sucesso';
-                        $scope.foto = {};
-                    })
-                    .error(function(erro) {
-                        console.log(erro);
-                        $scope.mensagem = 'Não foi possível cadastrar a foto';
-                    });
+                recursoFoto.save($scope.foto, function() {
+                    $scope.mensagem = 'Foto "'+$scope.foto.titulo+'" cadastrada com sucesso';
+                    $scope.foto = {};
+                },function(erro){
+                    console.log(erro);
+                    $scope.mensagem = 'Não foi possível cadastrar a foto';
+                });
             }
         }
     }; 

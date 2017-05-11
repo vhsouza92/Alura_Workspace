a471abef2679e0d7053717b37d990a4fc026db7a
Victor Hugo <vhsouza92@gmail.com>
2017-05-11 14:23:27 +0000

    Defalut

---
+++ Angular2/alurapic/server/server.js
@@ -0,0 +1,8 @@
+var http = require('http')
+    ,app = require('./config/express')
+    db = require('./config/database');
+
+http.createServer(app).listen(process.env.PORT, process.env.IP);
+
+console.log("Servidor escutando na porta:"+ process.env.PORT);
+console.log("IP: "+ process.env.IP);
